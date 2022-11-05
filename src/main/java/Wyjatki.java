6530c6013a42c65048a048e56bcf8dc7a4fa9962
Adam Toczek <a.a.toczek@gmail.com>
2022-11-05 14:28:07 +0100

    wyjatki

---
+++ src/main/java/Wyjatki.java
@@ -0,0 +1,16 @@
+public class Wyjatki {
+    public static void main(String[] args) {
+        int[] tab = {5,6,7,1,2};
+        try {
+
+            for (int i=0; ; i++)
+                System.out.println(tab[i]);
+
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("wyszedles poza tablice");
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
