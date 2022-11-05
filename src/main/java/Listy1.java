5137101d05b6968c54e3e54980aaa8ab27fd43e1
Adam Toczek <a.a.toczek@gmail.com>
2022-11-05 12:45:07 +0100

    listy i foreach

---
+++ src/main/java/Listy1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listy1 {
+    public static void main(String[] args) {
+        /*Stwórz listę liczb od 0 do 100. Pomnóż liczby parzyste przez 2.
+            Wydrukuj listę na konsoli.
+        */
+        List<Integer> listaLiczb = new ArrayList<>(); //listy operuja tylko na obiektach, dlatego zamiast int uzywam Integer
+
+        for (int i =0; i<=100; i++)
+            listaLiczb.add(i);
+
+        for (int i =2; i<=listaLiczb.size(); i+=2) { //iteruje sie co drugi element listy, zaczynajac od pierwszego parzystego
+            listaLiczb.set(i, listaLiczb.get(i)*2);
+        }
+
+        for (Integer i : listaLiczb) //petla forech
+            System.out.println(i);
+
+    }
+}
