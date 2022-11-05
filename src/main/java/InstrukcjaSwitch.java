f2a1a0d9bd712dbac32e17b05825949d96b4c7c2
Adam Toczek <a.a.toczek@gmail.com>
2022-11-05 11:04:54 +0100

    instrukcje warunkowe

--- src/main/java/InstrukcjaSwitch.java
+++ src/main/java/InstrukcjaSwitch.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class InstrukcjaSwitch {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj imie");
+        String imie = input.nextLine();
 
+        switch (imie) {
+            case "Patrycja":
+            case "Gabrysia" :
+            case "Janek" :
+            case "Mateusz":
+            case "Julek":
+            case "Natalia":
+            case "Basia":
+            case "Krystian":
+                System.out.println("Czesc " + imie);
+                break;
+            default:
+                System.out.println("Nie znam Cie :(");
+        }
     }
 }
---
+++ src/main/java/MagazynOwocow.java
@@ -0,0 +1,28 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class MagazynOwocow {
+    public static void main(String[] args) {
+        System.out.println("podaj nazwe owocow: ");
+        Scanner input = new Scanner(System.in);
+
+        String owoc = input.nextLine();
+        switch (owoc) {
+            case "jablka":
+            case "banany":
+            case "pomarancze":
+                System.out.println("podaj ilosc owocow:");
+                int waga = input.nextInt();
+                if (waga == 100 )
+                    System.out.println(owoc + " jest w sam raz");
+                else if (waga <100)
+                    System.out.println("jest jeszcze "+ (100-waga) +"kg miejsca na "+owoc);
+                else
+                    System.out.println(owoc + "jest za duzo o "+(waga-100)+"kg");
+                break;
+            default:
+                System.out.println(owoc + " nie ma w magazynie");
+
+        }
+    }
+}
